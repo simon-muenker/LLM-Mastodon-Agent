e1504c07faf91ddd60d8cc7279e3039735f08478
Simon Münker <muenker@uni-trier.de>
2024-07-18 16:30:27 +0200

    apply ruff reformat

--- examples/PEFT-Agent.py
+++ examples/PEFT-Agent.py
@@ -7,13 +7,11 @@ import llm_mastodon_agent as src
 ENV = dotenv.dotenv_values(".env")
 
 agent = src.Agent(
-    client=src.mastodon.Client(
-        name=ENV["MASTODON_USERNAME"], bearer=ENV["MASTODON_BEARER"]
-    ),
-    integration = src.integrations.Huggingface(
+    client=src.mastodon.Client(name=ENV["MASTODON_USERNAME"], bearer=ENV["MASTODON_BEARER"]),
+    integration=src.integrations.Huggingface(
         llm_slug=ENV["HUGGINGFACE_PEFT_PATH"],
         auth_token=ENV["HUGGINGFACE_AUTH"],
-    )
+    ),
 )
 
 topics = ["Trump", "Biden", "healthcare", "rifles", "voting age"]
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,9 @@ ENV = dotenv.dotenv_values(".env")
 
 @pytest.fixture(scope="session", autouse=True)
 def client() -> src.mastodon.Client:
-    return src.mastodon.Client(
-        name=ENV["MASTODON_USERNAME"], bearer=ENV["MASTODON_BEARER"]
-    )
+    return src.mastodon.Client(name=ENV["MASTODON_USERNAME"], bearer=ENV["MASTODON_BEARER"])
 
 
 @pytest.fixture(scope="session", autouse=True)
 def integration() -> src.integrations.WebAPI:
-    return src.integrations.WebAPI(
-        llm_slug=ENV["WEBAPI_MODEL"], api=ENV["WEBAPI_ENDPOINT"]
-    )
+    return src.integrations.WebAPI(llm_slug=ENV["WEBAPI_MODEL"], api=ENV["WEBAPI_ENDPOINT"])
