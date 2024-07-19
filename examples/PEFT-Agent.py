6746634dc6e651c3be28f549a57675935eb45e90
Simon Münker <simon.muenker@googlemail.com>
2024-07-19 09:01:31 +0000

    provide separate accounts for the agents

--- examples/PEFT-Agent.py
+++ examples/PEFT-Agent.py
@@ -1,31 +1,50 @@
-import time
-
 import dotenv
 
 import llm_mastodon_agent as src
 
-ENV = dotenv.dotenv_values(".env")
 
-agent = src.Agent(
-    client=src.mastodon.Client(
-        name=ENV["MASTODON_USERNAME"], bearer=ENV["MASTODON_BEARER"], api=ENV["MASTODON_API"]
-    ),
-    integration=src.integrations.Huggingface(
-        llm_slug=ENV["HUGGINGFACE_PEFT_PATH"],
-        auth_token=ENV["HUGGINGFACE_AUTH"],
-    ),
-    prompting=src.Prompting(
-        persona_description="You are a social media user.",
-        ideologies={
-            "right-leaning": "Your are politically right leaning. Reflect conservative values and opinions in your responses."
-        },
-        post_task="Write a tweet on the following topic: {topic}",
-    ),
+ENV = dotenv.dotenv_values(".env")
+TOPICS = [
+    "Position on Trump",
+    "Position on Biden",
+    "Position on healthcare",
+    "Position on rifles",
+    "Position on voting age",
+]
+
+
+prompting = src.Prompting(
+    persona_description="You are a social media user.",
+    ideologies={
+        "left-leaning": "You have politically left-leaning views. Reflect progressive values and opinions in your responses.",
+        "right-leaning": "Your have politically right-leaning views. Reflect conservative values and opinions in your responses.",
+    },
+    post_task="Write a tweet on the following topic: {topic}",
 )
 
-topics = ["Trump", "Biden", "healthcare", "rifles", "voting age"]
+agents = {
+    "left-leaning": src.Agent(
+        client=src.mastodon.Client(
+            name=ENV["MASTODON_LEFT_USERNAME"], bearer=ENV["MASTODON_LEFT_BEARER"], api=ENV["MASTODON_API"]
+        ),
+        integration=src.integrations.Huggingface(
+            llm_slug=ENV["HUGGINGFACE_AGENT_LEFT_PATH"],
+            auth_token=ENV["HUGGINGFACE_AUTH"],
+        ),
+        prompting=prompting,
+    ),
+    "right-leaning": src.Agent(
+        client=src.mastodon.Client(
+            name=ENV["MASTODON_RIGHT_USERNAME"], bearer=ENV["MASTODON_RIGHT_BEARER"], api=ENV["MASTODON_API"]
+        ),
+        integration=src.integrations.Huggingface(
+            llm_slug=ENV["HUGGINGFACE_AGENT_RIGHT_PATH"],
+            auth_token=ENV["HUGGINGFACE_AUTH"],
+        ),
+        prompting=prompting,
+    ),
+}
 
-for topic in topics:
-    agent.post(topic=topics, ideology="right-leaning", retrieve_news=False)
-
-    time.sleep(10)
+for topic in TOPICS:
+    for ideology, agent in agents.items():
+        agent.post(topic=topic, ideology=ideology, retrieve_news=False)
